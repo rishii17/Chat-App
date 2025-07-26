8e1cda646fa2b6554735c6f88e4c5dd2d7e13d24
rishii17 <127650997+rishii17@users.noreply.github.com>
2025-07-26 12:05:28 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export  const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:5001/api":"https://chat-app-ten-wheat-61.vercel.app/api",
+    baseURL:import.meta.env.MODE==="development"?"http://localhost:5001/api":"https://chat-app-gamma-pied-29.vercel.app/api",
     withCredentials:true,
 })
